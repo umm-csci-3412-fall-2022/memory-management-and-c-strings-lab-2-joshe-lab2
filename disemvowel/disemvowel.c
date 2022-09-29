0c50cd146f6822c830bbccd0542fc8e2dfd067a5
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-29 09:54:39 -0500

    Add creation of the result in disemvowel.c by taking every character that is not a vowel and placing it into the result; At the end, a \0 is placed.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -3,8 +3,21 @@
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  int length, index, i;
-  char *result
+  int index, i;
+  int length = strlen(str);
+  char *result = (char*) calloc(length + 1, sizeOf(char));
+	
+  // For every character in the inputed string, we:
+  // Check if it is not a vowel, if it is not:
+  // We put the resulting charater into the result 
+  // And we increase the size of the index
+  for (i=0; i<length; ++i) {
+    if (str[i].toLowerCase() != 'a' && str[i].toLowerCase() != 'e' && str[i].toLowerCase() != 'i' && str[i].toLowerCase() != 'o' && str[i].toLowerCase() != 'u') {
+      result[index] = str[i];
+      index++;
+    }	    
+  }
 
+  result[index] = '\0';
   return (char*) "";
 }
