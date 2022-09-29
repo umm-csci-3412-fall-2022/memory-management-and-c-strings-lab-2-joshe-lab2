4e5dce7d191e2725086003586cecc976c3337ac6
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-29 10:10:30 -0500

    Fix errors in disemvowel.c so that it builds without error

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -2,22 +2,24 @@
 
 #include "disemvowel.h"
 
+#include <string.h>
+
 char *disemvowel(char *str) {
   int index, i;
   int length = strlen(str);
-  char *result = (char*) calloc(length + 1, sizeOf(char));
+  char *result = (char*) calloc(length + 1, sizeof(char));
 	
   // For every character in the inputed string, we:
   // Check if it is not a vowel, if it is not:
   // We put the resulting charater into the result 
   // And we increase the size of the index
   for (i=0; i<length; ++i) {
-    if (str[i].toLowerCase() != 'a' && str[i].toLowerCase() != 'e' && str[i].toLowerCase() != 'i' && str[i].toLowerCase() != 'o' && str[i].toLowerCase() != 'u') {
+    if (str[i] != 'A' && str[i] != 'a' && str[i] != 'E' && str[i] != 'e' && str[i] != 'I' && str[i] != 'i' && str[i] != 'O' && str[i] != 'o' && str[i] != 'U' && str[i] != 'u') {
       result[index] = str[i];
       index++;
     }	    
   }
 
   result[index] = '\0';
-  return (char*) "";
+  return result;
 }
